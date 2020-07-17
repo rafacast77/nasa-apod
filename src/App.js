df26d597c46b36c7fd92de3787dbcebbd8bf62d3
rafacast77 <1600389@rgu.ac.uk>
2020-07-17 21:35:50 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import NasaPhoto from './components/Nasaphoto';
 import './App.css';
-
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Route component={Home} path="/" exact />
+        <Route component={Home} path="/nasa-apod" />
         <Route component={NasaPhoto} path="/nasaphoto" />
       </div>
     </BrowserRouter>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
+/** @format */
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
-    return (
-        <div className="navbar">
-            <ul>
-                <Link className="link" to="/" exact >Houston get me home</Link>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <ul>
+        <Link className="link" to="/nasa-apod">
+          Houston get me home
+        </Link>
+      </ul>
+    </div>
+  );
 }
