5e68422cd19c7532b5ca2b6ec3a6fa9dd264d683
rafacast77 <1600389@rgu.ac.uk>
2020-07-17 14:58:53 +0100

    Updates

---
+++ precache-manifest.7685ea6e9fa06d38b1f302fa9382a112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9fab048fb7bf6119686929114b4f5c",
+    "url": "/nasa-apod/index.html"
+  },
+  {
+    "revision": "2ed2aa09626d992785e8",
+    "url": "/nasa-apod/static/css/main.a5781c35.chunk.css"
+  },
+  {
+    "revision": "779def5278181034208c",
+    "url": "/nasa-apod/static/js/2.6e877178.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-apod/static/js/2.6e877178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed2aa09626d992785e8",
+    "url": "/nasa-apod/static/js/main.fc399298.chunk.js"
+  },
+  {
+    "revision": "f14b7405e8c81cf6f152",
+    "url": "/nasa-apod/static/js/runtime-main.2852ea26.js"
+  }
+]);
